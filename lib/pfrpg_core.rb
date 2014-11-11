2af43bd67db0a07068389ad7b8f364e34e406d4c
Jordan OMara <jomara@redhat.com>
2014-11-11 17:59:44 -0500

    Fixing import & bumping

--- lib/pfrpg_core.rb
+++ lib/pfrpg_core.rb
@@ -1,3 +1,6 @@
+module PfrpgCore
+end
+
 require 'pfrpg_core/effect'
 require 'pfrpg_core/effect_processor'
 require 'pfrpg_core/math_helper'
@@ -5,7 +8,4 @@ require 'pfrpg_core/null_object'
 
 Dir[File.expand_path(File.join(File.dirname(File.absolute_path(__FILE__)), 'pfrpg_core/')) + "/**/*.rb"].each do |file|
   require file
-end
-
-module PfrpgCore
 end
--- lib/pfrpg_core/version.rb
+++ lib/pfrpg_core/version.rb
@@ -1,3 +1,3 @@
 module PfrpgCore
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
