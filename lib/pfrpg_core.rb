b28c35543cb7b035343e9a6e3ea0aea1994a4eee
Jordan OMara <jomara@redhat.com>
2014-11-13 14:14:36 -0500

    Adding dice

--- lib/pfrpg_core.rb
+++ lib/pfrpg_core.rb
@@ -2,6 +2,7 @@ module PfrpgCore
 end
 
 require 'pfrpg_core/alignment'
+require 'pfrpg_core/dice'
 require 'pfrpg_core/effect'
 require 'pfrpg_core/effect_processor'
 require 'pfrpg_core/math_helper'
