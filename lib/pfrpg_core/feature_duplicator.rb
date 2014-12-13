cec6aaf4253e2ce89fe8e699ed4268f21f9a0ffe
Jordan OMara <jomara@redhat.com>
2014-12-13 18:58:59 -0500

    Removing macro tests, adding feature duplicator

---
+++ lib/pfrpg_core/feature_duplicator.rb
@@ -0,0 +1,26 @@
+module PfrpgCore::FeatureDuplicator
+
+  def dupe_words
+    ['Sneak Attack', 'Channel Energy', 'Damage Reduction', 'Trap Sense']
+  end
+
+  # filter things with multiple bonuses, like sneak attack and channel energy
+  def filter_duplicates(features)
+    dupe_words.each do |w|
+      features = features - smallest(features, w)
+    end
+    features
+  end
+
+  def smallest(features, phrase)
+    smallest = features.select { |x| x.name[phrase] != nil }
+    return [] if (smallest.empty? || smallest.size == 1)
+    smallest.sort_by! { |x| parse_number(x.name) }
+    smallest[0..smallest.size-2]
+  end
+
+  # of the form, 'Sneak Attack (10d6)'
+  def parse_number(description)
+    (description.split('d6')[0].split(' ').last.split('(')[1]).to_i
+  end
+end
