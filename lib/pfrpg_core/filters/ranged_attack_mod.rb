c7729ff2d9bf5e80e6bf7d701463a689b7f3f084
Jordan OMara <jsomara@gmail.com>
2015-08-16 15:13:25 -0700

    Bump .15, dont apply ranged bonus for < 10ft of range

--- lib/pfrpg_core/filters/ranged_attack_mod.rb
+++ lib/pfrpg_core/filters/ranged_attack_mod.rb
@@ -13,7 +13,7 @@ module PfrpgCore
       def modify_attack_for_ranged_attack(attack, dex, str)
         begin
           ranged = attack.range.to_i
-          if !(ranged == nil || ranged == '' || ranged == 0)
+          if !(ranged == nil || ranged == '' || ranged <= 10)
             attack.filter_str << "Range Bonus: -#{dex}, +#{str}"
             attack.other_bonus += dex
             attack.other_bonus -= str
