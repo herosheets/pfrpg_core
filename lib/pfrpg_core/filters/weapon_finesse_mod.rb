18d244cbca8608287e0328f80929df9842014257
Jordan OMara <jsomara@gmail.com>
2015-08-16 14:52:37 -0700

    Add Rapier to list of supported weapons in Weapon Finesse - it is not a light weapon but still gets weapon finesse

--- lib/pfrpg_core/filters/weapon_finesse_mod.rb
+++ lib/pfrpg_core/filters/weapon_finesse_mod.rb
@@ -13,7 +13,7 @@ module PfrpgCore
       end
 
       def modify_attack_for_weapon_finesse(attack, dex, str)
-        if attack.weight_class == 'light' || attack.weight_class == 'natural'
+        if attack.weight_class == 'light' || attack.weight_class == 'natural' || attack.weapon_name['Rapier']
           attack.filter_str << "Weapon Finesse: -#{dex}, +#{str}"
           attack.other_bonus += dex
           attack.other_bonus -= str
